63ef17a9c91ffc432fe59906f78e98ab84b6421e
MikhailVinogradov224 <m89119750476@gmail.com>
2022-06-09 21:56:21 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+ 
+namespace ConsoleApplication
+{
+    class Program
+    {
+        
+        public static UInt64 AckermannFunc(UInt64 m, UInt64 n) {
+            
+            if (m == 0)
+                return n + 1;
+            if (m > 0)
+                if (n == 0)
+                    return AckermannFunc(m - 1, 1);
+            return AckermannFunc(m - 1, AckermannFunc(m, n - 1));
+        }
+        
+        public static void Main(string[] args) {
+            Console.WriteLine(AckermannFunc(2, 3));
+            
+        }
+    }
+}
